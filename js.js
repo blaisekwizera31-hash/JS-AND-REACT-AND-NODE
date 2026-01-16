c4013a8350a85a274310331a9f5a1c15da15c772
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-16 08:06:08 +0200

    commited

--- js.js
+++ js.js
@@ -1,51 +1,69 @@
-function sayHello(){
-    console.log("Hi")
-}
+// function sayHello(){
+//     console.log("Hi")
+// }
 
-function doSomething(callBack){
-    console.log("I am doing something ...");
-    callBack();
-}
-doSomething(sayHello);
+// function doSomething(callBack){
+//     console.log("I am doing something ...");
+//     callBack();
+// }
+// doSomething(sayHello);
 
-const numbers = [1, 2, 3 ,4 ,5];
-const doubled = numbers.map(function(num) {
-    return num * 2;
-});
- console.log(doubled);
+// const numbers = [1, 2, 3 ,4 ,5];
+// const doubled = numbers.map(function(num) {
+//     return num * 2;
+// });
+//  console.log(doubled);
 
-const evens = numbers.map(function(num){
-    return num%2 ==0;
-});
-console.log(evens) ;
+// const evens = numbers.map(function(num){
+//     return num%2 ==0;
+// });
+// console.log(evens) ;
 
-const even = numbers.filter(function(num){
-    return num%2 ==0;
-});
+// const even = numbers.filter(function(num){
+//     return num%2 ==0;
+// });
 
-console.log(even);
+// console.log(even);
 
-const odds = numbers.filter(function(number){
-    return number %2 != 0;
-});
-console.log(odds);
+// const odds = numbers.filter(function(number){
+//     return number %2 != 0;
+// });
+// console.log(odds);
 
-var http = require('http');
-var server = http.createServer(function(req, res){
+// var http = require('http');
+// var server = http.createServer(function(req, res){
 
-    var log = {
-        function info(info){
-            console.log('info:' + info);
-        },
-        function warning(warning){
-            console.log('warning: ' + warning)
-        }
+//     var log = {
+//         function info(info){
+//             console.log('info:' + info);
+//         },
+//         function warning(warning){
+//             console.log('warning: ' + warning)
+//         }
+//     }
+// });
+
+// server.listen(3000);
+// module.exports = log
+
+// for( i=0; i<6; i++){
+//     console.log("Hello world");
+// }
+
+const identity = {
+    names: 'blaise',
+    age : 30
+};
+
+for(let key in identity){
+    for(let value in identity){
+        console.log(key + " : " + value)
     }
-});
-
-server.listen(3000);
-module.exports = log
-
-for( i=0; i<6; i++){
-    console.log("Hello world");
 }
+function walk(){
+    console.log("walking");
+
+}
+
+walk()
+console.log(walk());
